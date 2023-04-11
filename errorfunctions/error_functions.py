b6c961fde41fbf1ade6d9197013744bd7d500280
Tharaka Umayanga <tharakau@gmail.com>
2023-04-11 10:21:03 +0530

    feat[issue#1]:Added sigmoid to ErrorFunctions

--- errorfunctions/error_functions.py
+++ errorfunctions/error_functions.py
@@ -1,4 +1,20 @@
+"""
+    This module contains ErrorFunctions class.
+    """
+import numpy as np
+
+
 class ErrorFunctions:
     """
     This class contains error functions for neural networks and machine learning algorithms
     """
+
+    def __init__(self, input_no):
+        self.input_no = input_no
+
+    def sigmoid(self):
+        """
+        return sigmoid value for given input
+        y=1/(1+e^(-x))
+        """
+        return 1 / (1 + np.exp(self.input_no))
