eeb82903ed9788cecc2e16ca9c3c75f7e26e824b
Tharaka Umayanga <tharakau@gmail.com>
2023-04-11 16:06:59 +0530

    feat[issue#1]:Added type checking to ErrorFunctions class

--- errorfunctions/error_functions.py
+++ errorfunctions/error_functions.py
@@ -11,6 +11,12 @@ class ErrorFunctions:
     """
 
     def __init__(self, input_no):
+        if type(input_no) != float:
+            try:
+                input_no = float(input_no)
+            except ValueError:
+                raise TypeError(
+                    f"Only integers and floats are available.\nType mismatch expected int/float received {type(input_no)}")
         self.input_no = input_no
 
     def sigmoid(self):
@@ -18,4 +24,6 @@ class ErrorFunctions:
         return sigmoid value for given input
         y=1/(1+e^(-x))
         """
-        return np.round(1 / (1 + np.exp(-self.input_no)),8)
+        return np.round(1 / (1 + np.exp(-self.input_no)), 8)
+
+
