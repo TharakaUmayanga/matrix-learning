df1cc78ea322e17db018f2d0be0f02da64f87e2c
Tharaka Umayanga <tharakau@gmail.com>
2023-04-14 13:00:20 +0530

    feat[issue#1]: added ErrorFunctions class and mse function

--- optimizations/error_functions.py
+++ optimizations/error_functions.py
@@ -10,9 +10,18 @@ class ErrorFunctions:
     for neural networks and machine learning algorithms.
     """
 
-    def __init__(self, input_no):
-        if type(input_no) != np.ndarray:
-            raise TypeError(f"expected numpy.ndarray received {type(input_no)}")
-        self.input_no = input_no
+    def __init__(self, actual, predicted):
+        if type(actual) != np.ndarray or type(predicted) != np.ndarray:
+            raise TypeError(f"expected numpy.ndarray received {type(actual)}")
+        elif len(actual) != len(predicted):
+            raise RuntimeError("actual and predicted values should have same length\n"
+                               f"actual len {len(actual)} predicted len "
+                               f"{len(predicted)}")
+        self.actual = actual
+        self.predicted = predicted
 
-
+    def mse(self):
+        """
+        :return: returns mean squared error for the input value
+        """
+        return np.power(self.actual - self.predicted, 2) / len(self.actual)
