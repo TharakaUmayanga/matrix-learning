fa2810025f96af8134ccb0347c53d4cfe0c462b1
Tharaka Umayanga <tharakau@gmail.com>
2023-04-14 17:16:18 +0530

    feat[issue#1]: added mae unit tests

--- unittests/test_error_functions.py
+++ unittests/test_error_functions.py
@@ -33,3 +33,16 @@ class TestErrorFunctions(TestCase):
         testing.assert_array_almost_equal(test1.mse(), np.array([0.08250]))
 
         testing.assert_array_almost_equal(test2.mse(), np.array([29.02520]))
+
+    def test_mae(self):
+        self.true_y_1 = np.array([0.223, 0.334, 0.554, 0.665])
+        self.predicted_y_1 = np.array([0.323, 0.734, 0.154, 0.665])
+
+        self.true_y_2 = np.array([-1.223, 0.934, -0.554, 0.165])
+        self.predicted_y_2 = np.array([-1.323, 0.0734, 10.154, -0.665])
+
+        test1 = ErrorFunctions(self.true_y_1, self.predicted_y_1)
+        test2 = ErrorFunctions(self.true_y_2, self.predicted_y_2)
+        testing.assert_array_almost_equal(test1.mae(), np.array([0.22500]))
+
+        testing.assert_array_almost_equal(test2.mae(), np.array([3.12465]))
